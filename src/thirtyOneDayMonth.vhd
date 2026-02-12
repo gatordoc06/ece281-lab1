e746e201c7da149db757e922838c8b5b87e0afb8
Lopez <c28elias.lopez@afacademy.af.edu>
2026-02-11 19:39:57 -0700

    Lab 1 Code

--- src/thirtyOneDayMonth.vhd
+++ src/thirtyOneDayMonth.vhd
@@ -56,16 +56,17 @@ library ieee;
 entity thirtyOneDayMonth is 
   port(
 	i_A : in std_logic; -- one of four inputs
-	
-	
-	
+	i_B : in std_logic;
+	i_C : in std_logic;
+	i_D : in std_logic;
 						-- output
+	o_Y : out std_logic	
   );
 end thirtyOneDayMonth;
 
 architecture thirtyOneDayMonth_arch of thirtyOneDayMonth is 
 	-- include components declarations and signals
-	
+	signal w_sel : std_logic_vector (2 downto 0); -- MUX sel
 	--signals internal to the architecture are declared and initialized such as w_sel
   
 begin
@@ -73,7 +74,19 @@ begin
 	--assigning names to reflect original schematics (for ease of understanding if you wish to)
 	w_sel(0) <= i_C;	-- one
 	--finish assigning signals
-	
+	w_sel(1) <= i_B;
+	w_sel(2) <= i_A;
 	--enter your logic here to implement the mux.  See VHDL reference sheet for MUX syntax.	
+	o_y <= '1' when (w_sel = "000" and i_d = '1') else 
+	       '1' when (w_sel = "001" and i_d = '1') else 
+	       '1' when (w_sel = "010" and i_d = '1') else 
+	       '1' when (w_sel = "011" and i_d = '1') else
+	       '1' when (w_sel = "100" and i_d = '0') else
+	       '1' when (w_sel = "101" and i_d = '0') else
+	       '1' when (w_sel = "110" and i_d = '0') else
+	       '0'; 
+	    
+	       
+	
 	---------------------------------------------------------------	
 end thirtyOneDayMonth_arch;
